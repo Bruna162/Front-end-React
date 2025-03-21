cb7c12e58a99110a3aa8bcaa96c9892ae6ce6053
Bruna162 <167482118+Bruna162@users.noreply.github.com>
2025-03-21 14:50:23 -0300

    Banco ThenList

---
+++ Banco ThenList.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE banco_anime;
+
+USE banco_anime;
+
+CREATE TABLE generos (
+    id_genero INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE usuarios (
+    id_usuarios INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(20) NOT NULL,
+    senha VARCHAR(30) NOT NULL,
+    email VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE animes (
+    id_anime INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    titulo VARCHAR(255) NOT NULL,
+    sinopse TEXT,
+    estudio VARCHAR(255),
+    imagem_url VARCHAR(500),
+    episodios_totais INT NOT NULL,
+    genero_id INT,
+    FOREIGN KEY (genero_id) REFERENCES generos(id_genero) ON DELETE CASCADE
+);
+
+CREATE TABLE listas_usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT,
+    anime_id INT,
+    status ENUM('Quero Assistir', 'Assistindo', 'Concluído', 'Abandonado') NOT NULL,
+    episodios_assistidos INT DEFAULT 0,
+    adicionado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id_usuarios) ON DELETE CASCADE,
+    FOREIGN KEY (anime_id) REFERENCES animes(id_anime) ON DELETE CASCADE
+);
